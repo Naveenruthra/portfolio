37ee62db0af9ed2a3599bdb993e8058c49acdfd2
Naveen Kumar. L <www.naveenrazer@gmail.com>
2025-07-20 14:48:31 +0000

    i created a separate branch to production now do the changes to host in

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
   // and replace 'your-repo-name' with the name of your GitHub repository.
   // For example, if your repository is https://github.com/user/my-portfolio,
   // then basePath should be '/my-portfolio'.
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name/',
+  basePath: '/your-repo-name',
+  assetPrefix: '/your-repo-name/',
   typescript: {
     ignoreBuildErrors: true,
   },
