aedf90a9b99d62e6205a76be6b50305570a4e3cb
Naveenkumar L <68478478+Naveenruthra@users.noreply.github.com>
2025-07-22 09:32:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  /* config options here */
   output: 'export',
   basePath: '/portfolio',
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
